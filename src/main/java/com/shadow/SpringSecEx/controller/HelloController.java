59570997f1eaddc0e769c17db714bd4a6ea3aae3
csbiyyala <chandrashekarbiyyala@gmail.com>
2026-09-02 15:30:30 +0530

    implemented basic security filter chain

--- src/main/java/com/shadow/SpringSecEx/controller/HelloController.java
+++ src/main/java/com/shadow/SpringSecEx/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.shadow.SpringSecEx.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello, World!";
+    public String hello(HttpServletRequest request) {
+        return "Hello, World!, Session ID: " + request.getSession().getId();
     }
 }
