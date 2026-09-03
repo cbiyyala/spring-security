6f9806639a4b78ad5d573efbc75ff20165086cdb
csbiyyala <chandrashekarbiyyala@gmail.com>
2026-09-03 10:05:04 +0530

    added bCrypt encoder for passoword during login

--- src/main/java/com/shadow/SpringSecEx/config/SecurityConfig.java
+++ src/main/java/com/shadow/SpringSecEx/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -35,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
-        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
         // authProvider.setUserDetailsService(userDetailsService);
         return authProvider;
     }
